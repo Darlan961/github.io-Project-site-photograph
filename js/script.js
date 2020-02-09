2323db064d45f4ef4c4f321541845f74f3f1e4e2
José Darlan <josedarlan961@gmail.com>
2020-02-09 16:47:51 -0300

    Liks para redes socias concluidos, prefixos css mobile e desktop inseridos

--- js/script.js
+++ js/script.js
@@ -1,31 +1,18 @@
+/*--*/
+
+//Estilizando o fomulário de contato. 
 let input = document.querySelectorAll('#form-values input, #form-values select, #form-values textarea');
 
+for (let i in input) {
 
-
-
-let functionStyleFormContact = () => {
-
-    for (let i in input) {
-
-        console.log(input[i])
-
-        input[i].onfocus = function () {
-            input[i].style.backgroundColor = '#fff';
-            input[i].style.borderBottom = '1px solid #ff3589';
-        }
-
+    // console.log(input[i])
+    input[i].onfocus = function () {
+        input[i].style.backgroundColor = '#fff';
+        input[i].style.borderBottom = '1px solid #ff3589';
     }
 
+    input[i].onblur = function () {
+        input[i].style.border = 'none';
+        input[i].style.backgroundColor = '#f0eaea';
+    }
 }
-
-functionStyleFormContact();
-
-
-
-
-
-
-
-// input.addEventListener('focus', )
-
-//    border-bottom: 1px solid #000;
